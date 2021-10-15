ea3ed2c4fd25eb52a19a0cf71ae655525dc5f18c
Tabish Shaikh <tabish@archive.org>
2021-10-15 22:57:23 +0530

    tests: test_config updated for strategy

--- tests/functional/strategy/test_config.py
+++ tests/functional/strategy/test_config.py
@@ -3,11 +3,6 @@ from brownie import *
 
 from helpers.constants import AddressZero
 
-# setPerformanceFeeStrategist
-# setPerformanceFeeGovernance
-# setVault
-# setWithdrawalMaxDeviationThreshold
-
 ## Test Permissioned actions
 def test_setGuardian(deploy_complete, governance, rando):
 
@@ -57,3 +52,52 @@ def test_setPerformanceFeeStrategist(deploy_complete, governance, rando):
     # setting more that MAX_FEE should fail
     with brownie.reverts("base-strategy/excessive-strategist-performance-fee"):
         strategy.setPerformanceFeeStrategist(2 * strategy.MAX_FEE(), {"from": governance})
+
+def test_setPerformanceFeeGovernance(deploy_complete, governance, rando):
+
+    strategy = deploy_complete.strategy
+    performanceFeeGovernance = 2000 # Good Strategist become have more rare therefore to compensate them more
+
+    # setPerformanceFeeGovernance from random user should fail
+    with brownie.reverts("onlyGovernance"): 
+        strategy.setPerformanceFeeGovernance(performanceFeeGovernance, {"from": rando})
+
+    # setting setPerformanceFeeGovernance
+    strategy.setPerformanceFeeGovernance(performanceFeeGovernance, {"from": governance})
+
+    assert strategy.performanceFeeGovernance() == performanceFeeGovernance
+
+    # setting more that MAX_FEE should fail
+    with brownie.reverts("base-strategy/excessive-governance-performance-fee"):
+        strategy.setPerformanceFeeGovernance(2 * strategy.MAX_FEE(), {"from": governance})
+
+def test_setVault(deploy_complete, governance, rando):
+
+    strategy = deploy_complete.strategy
+
+    # setVault from random user should fail
+    with brownie.reverts("onlyGovernance"): 
+        strategy.setVault(rando, {"from": rando})
+
+    # setting vault address
+    strategy.setVault(rando, {"from": governance})
+
+    assert strategy.vault() == rando
+
+def test_setWithdrawalMaxDeviationThreshold(deploy_complete, governance, rando):
+
+    strategy = deploy_complete.strategy
+    withdrawalMaxDeviationThreshold = 100
+
+    # withdrawalMaxDeviationThreshold from random user should fail
+    with brownie.reverts("onlyGovernance"): 
+        strategy.setWithdrawalMaxDeviationThreshold(withdrawalMaxDeviationThreshold, {"from": rando})
+
+    # setting withdrawalMaxDeviationThreshold
+    strategy.setWithdrawalMaxDeviationThreshold(withdrawalMaxDeviationThreshold, {"from": governance})
+
+    assert strategy.withdrawalMaxDeviationThreshold() == withdrawalMaxDeviationThreshold
+
+    # setting more that MAX_FEE should fail
+    with brownie.reverts("base-strategy/excessive-max-deviation-threshold"):
+        strategy.setWithdrawalMaxDeviationThreshold(2 * strategy.MAX_FEE(), {"from": governance})
