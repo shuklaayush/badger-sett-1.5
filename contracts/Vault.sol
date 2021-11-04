936ff2256bc2f4e1c09bb5ba4789cdad61366d32
Entreprenerd <valeranijr@gmail.com>
2021-11-04 15:52:10 +0100

    feat: reuse `_mintSharesFor`

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -457,14 +457,7 @@ contract Vault is ERC20Upgradeable, SettAccessControl, PausableUpgradeable {
         uint256 _before = token.balanceOf(address(this));
         token.safeTransferFrom(msg.sender, address(this), _amount);
         uint256 _after = token.balanceOf(address(this));
-        _amount = _after.sub(_before); // Additional check for deflationary tokens
-        uint256 shares = 0;
-        if (totalSupply() == 0) {
-            shares = _amount;
-        } else {
-            shares = (_amount.mul(totalSupply())).div(_pool);
-        }
-        _mint(recipient, shares);
+        _mintSharesFor(recipient, _amount, _pool);
     }
 
     function _depositWithAuthorization(uint256 _amount, bytes32[] memory proof) internal virtual {
