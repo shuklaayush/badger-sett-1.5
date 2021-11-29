b4ea991292845463cef41f4afd709d1e96d015cc
Entreprenerd <valeranijr@gmail.com>
2021-11-29 21:34:49 +0100

    chore: delete unused file

--- tests/vault_strat_operation.py
+++
@@ -1,6 +0,0 @@
-# verify all funds deployed - available is invested
-# deposit on test strategy is called
-# cycle works as expected
-
-### See Fees, management, performance, withdrawal
-# fees test
